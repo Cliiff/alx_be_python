3101a3d22534e3da1ff09c6d5b67a66ab6fa0437
Cliiff <wicliffonyango9@gmail.com>
2025-07-13 23:29:20 +0300

    Add a simple future_age_calculator.py under python_introduction

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+input("How old are you?")
+age = int (input("How old are you? "))
+years_to_2050 = 2050 - 2023
+future_age = age + years_to_2050
+print("In 2050, you will be", future_age, "years old.")
